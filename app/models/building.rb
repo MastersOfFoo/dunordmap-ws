cd8d104df8296baa6954a04bd5fa408885eb9104
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-11-02 21:02:29 -0500

    Cache buildings information locally

--- app/models/building.rb
+++ app/models/building.rb
@@ -15,12 +15,15 @@ class Building
 
   def self.near(lat, lon)
     buildings = []
-    response = RestClient.get("#{SERVER_URL}/HelloWorld/resources/Uninorte/getCercanos?pos=#{lat}%20#{lon}")
+    response = RestClient.get("#{SERVER_URL}/HelloWorld/resources/Uninorte/getCercanos?pos=#{lon}%20#{lat}")
     buildings_info = JSON.parse(response)
     buildings_info.each do |building_id|
-      resp = RestClient.get("#{SERVER_URL}/HelloWorld/resources/Uninorte/getInformacion?id=#{building_id}")
-      building_info = JSON.parse(resp)
-      buildings << Building.new(name: building_info["nombre"], updated_at: Time.now, photo_url: building_info["foto"])
+      building = Rails.cache.fetch("buildings_#{building_id}", :expires_in => 1.day) do
+        resp = RestClient.get("#{SERVER_URL}/HelloWorld/resources/Uninorte/getInformacion?id=#{building_id}")
+        building_info = JSON.parse(resp)
+        Building.new(name: building_info["nombre"], updated_at: Time.now, photo_url: building_info["foto"])
+      end
+      buildings << building
     end
     buildings
   end
